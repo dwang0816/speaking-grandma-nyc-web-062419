b1c8c472aed3cfefe14659dd0acf3284ab83cc30
dennis wang <dwang0816@gmail.com>
2019-06-06 05:43:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 def speak_to_grandma(speak)
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
-  else
-    "HUH?! SPEAK UP, SONNY!"
   elsif speak == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
